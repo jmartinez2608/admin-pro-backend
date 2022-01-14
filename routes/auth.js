366d3cb3bf500bb5d6867be1c7a4928081d5eabe
Jose Martinez <jmart2608@gmail.com>
2022-01-14 20:59:37 -0300

    Sección 12 - Google

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@
 PATH: 'api/login'
 */
 const { Router } = require('express');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 const { check } = require('express-validator');
 const { validarCampos } = require('../middlewares/validar-campos');
 
@@ -17,6 +17,12 @@ router.post( '/', [
 
 
 ], login);
+
+
+router.post( '/google', [
+    check('token','El token es obligatorio').notEmpty(),    
+    validarCampos
+], googleSignIn);
 
 
 module.exports = router;
